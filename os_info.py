530c50fc6c0a4a2d6e6dcfe4c299421d31ae83f1
Aleksandr Drachilouski <AleksSkrydg@yandex.by>
2021-05-21 11:33:57 +0300

    os_info

---
+++ os_info.py
@@ -0,0 +1,8 @@
+import sys
+import platform
+
+info ='Os info is \n {}\n\nPython version is {} {}'.format(platform.uname(),sys.version,platform.architecture())
+print(info)
+
+with open('os_info.txt','w') as ff:
+    ff.write(info)
